4abe761c7507a8f01099f7d741f97c3113c83a69
euss95 <eugenioalcantara1995@gmail.com>
2019-05-01 17:37:02 +0200

    Trabajo asignatura

---
+++ elecciones/contracts/Elecciones.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Elecciones {
+	// Leer/escribir candidato
+    string public candidato;
+
+    // Constructor
+    constructor() public {
+        candidato = "Candidato 1";
+    } 
+}
