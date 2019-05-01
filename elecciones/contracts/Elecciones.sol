afd72fafa938eb99be8961b06ae1e9efc379db50
euss95 <eugenioalcantara1995@gmail.com>
2019-05-01 18:04:27 +0200

    2 candidatos

--- elecciones/contracts/Elecciones.sol
+++ elecciones/contracts/Elecciones.sol
@@ -1,11 +1,29 @@
 pragma solidity ^0.5.0;
 
 contract Elecciones {
-	// Leer/escribir candidato
-    string public candidato;
+	// Moderar candidato
+	struct Candidato {
+		uint id;
+		string nombre;
+		uint voteCount;
+	}
+
+	// Almacenar candidatos
+
+	// Obtener candidatos
+	mapping(uint => Candidato) public candidatos;
+
+	// Número candidatos
+	uint public candidatosContador; 
 
     // Constructor
     constructor() public {
-        candidato = "Candidato 1";
+    	aniadirCandidato("Candidato 1");
+    	aniadirCandidato("Candidato 2");
     } 
+
+    function aniadirCandidato (string memory _nombre) private {
+        candidatosContador ++;
+        candidatos[candidatosContador] = Candidato(candidatosContador, _nombre, 0);
+    }
 }
